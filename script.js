dc120d9f381d7a547ec5d3dfb37e42961cdf2b97
d2038 <35503394+d2038@users.noreply.github.com>
2023-02-08 13:28:28 -0400

    Add Game, Gameboard and Player objects

---
+++ script.js
@@ -0,0 +1,11 @@
+const Game = (() => {
+  const displayController = () => {};
+  return { displayController };
+})();
+
+const Gameboard = (() => {
+  const state = [];
+  return { state };
+})();
+
+const Player = (mark) => ({ mark });
