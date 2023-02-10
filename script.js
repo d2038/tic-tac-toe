054068457f53493b0c9e12167f5a4f5ffd3aab26
d2038 <35503394+d2038@users.noreply.github.com>
2023-02-10 15:07:10 -0400

    Add displayController render function

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const Game = (() => {
-  const displayController = () => {};
+  const displayController = () => {
+    const gameboardDOM = document.getElementsByClassName('square');
+    const render = (gameboardState) => {
+      for (const cell of gameboardDOM) {
+        const { index } = cell.dataset;
+        cell.innerText = gameboardState[index] || '';
+      }
+    };
+    return { render };
+  };
   return { displayController };
 })();
 
 const Gameboard = (() => {
-  const state = [];
+  const state = ['X', , 'O', 'O', , 'X', 'O', ,];
   return { state };
 })();
 
 const Player = (mark) => ({ mark });
+
+Game.displayController().render(Gameboard.state);
