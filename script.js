c844f5589e9c7e6f5b9ccff6c1573f62118c8ee1
d2038 <d2038.2011@gmail.com>
2023-02-13 17:15:38 -0400

    Add checkWin switchTurn functions

--- script.js
+++ script.js
@@ -23,11 +23,37 @@ const Gameboard = (() => {
     });
   };
 
+  const checkWin = () => {
+    const winArrays = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winArrays.forEach((combo) => {
+      if (
+        boardArray[combo[0]]
+        && boardArray[combo[0]] === boardArray[combo[1]]
+        && boardArray[combo[0]] === boardArray[combo[2]]
+      ) {
+        winner = 'current';
+      }
+    });
+
+    return winner || (boardArray.includes(undefined) ? null : 'Tie');
+  };
+
   return {
     boardArray,
     gameboard,
     cells,
     render,
+    checkWin,
   };
 })();
 
@@ -36,12 +62,18 @@ const Game = (() => {
   let playerOne;
   let playerTwo;
 
+  const switchTurn = () => {
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+  };
+
   const gameRound = () => {
     const board = Gameboard;
 
     board.gameboard.addEventListener('click', (event) => {
       if (event.target.className !== 'cell') return;
       const play = currentPlayer.playTurn(board, event.target);
+      if (!play) return;
+      const winStatus = board.checkWin();
     });
   };
 
