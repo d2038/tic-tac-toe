28b880ce7e3516986befa0311fa7de768460d64c
d2038 <35503394+d2038@users.noreply.github.com>
2023-02-13 14:11:58 -0400

    Add playTurn gameInit gameRound functions

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
-const Player = (mark) => ({ mark });
+const Player = (mark) => {
+  const playTurn = (board, cell) => {
+    const idx = cell.dataset.index;
+    if (board.boardArray[idx] === undefined) {
+      board.boardArray[idx] = mark;
+      board.render();
+      return true;
+    }
+    return false;
+  };
+
+  return { mark, playTurn };
+};
 
 const Gameboard = (() => {
-  const board = ['X', , 'O', 'O', , 'X', 'O', ,];
+  const boardArray = new Array(9);
+  const gameboard = document.querySelector('.gameboard');
   const cells = Array.from(document.querySelectorAll('.cell'));
 
   const render = () => {
-    board.forEach((mark, idx) => {
+    boardArray.forEach((mark, idx) => {
       cells[idx].textContent = mark;
     });
   };
-  return { board, render };
+
+  return {
+    boardArray,
+    gameboard,
+    cells,
+    render,
+  };
 })();
 
-const Game = (() => ({}))();
+const Game = (() => {
+  let currentPlayer;
+  let playerOne;
+  let playerTwo;
 
-Gameboard.render();
+  const gameRound = () => {
+    const board = Gameboard;
+
+    board.gameboard.addEventListener('click', (event) => {
+      if (event.target.className !== 'cell') return;
+      const play = currentPlayer.playTurn(board, event.target);
+    });
+  };
+
+  const gameInit = () => {
+    playerOne = Player('X');
+    playerTwo = Player('O');
+    currentPlayer = playerOne;
+
+    gameRound();
+  };
+
+  return { gameInit };
+})();
+
+Game.gameInit();
